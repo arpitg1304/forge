3c98caf9eb02583251b203d43f7b50c016618619
Arpit Gupta <arpit.wpi@gmail.com>
2026-05-26 20:53:58 -0400

    list local datasets using forge local

--- forge/hub/__init__.py
+++ forge/hub/__init__.py
@@ -14,18 +14,28 @@ Usage:
 """
 
 from forge.hub.download import (
+    CachedDataset,
     download_dataset,
     find_in_hf_cache,
+    find_in_lerobot_cache,
     get_cache_dir,
     get_hf_cache_dir,
+    get_lerobot_cache_dir,
+    list_hf_cache_datasets,
+    list_local_datasets,
 )
 from forge.hub.url import is_hf_url, parse_hf_url
 
 __all__ = [
+    "CachedDataset",
     "download_dataset",
     "find_in_hf_cache",
+    "find_in_lerobot_cache",
     "get_cache_dir",
     "get_hf_cache_dir",
+    "get_lerobot_cache_dir",
     "is_hf_url",
+    "list_hf_cache_datasets",
+    "list_local_datasets",
     "parse_hf_url",
 ]
